e4c3cb1a029437d55434ff5c386466765f9152df
582302894 <582302894@qq.com>
2018-06-27 18:03:55 +0800

    leet-code 旋转数组 https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/1/array/23/

--- lcode/simple_algorithm/rotate.php
+++ lcode/simple_algorithm/rotate.php
@@ -82,25 +82,20 @@ class Solution {
 0ms
 class Solution {
     public void rotate(int[] nums, int k) {
-       if (nums.length != 0 && k >= 0) {
-            k = k % nums.length;
-            reverse(nums, 0, nums.length - 1);
-            reverse(nums, 0, k - 1);
-            reverse(nums, k, nums.length - 1);
+        int n = nums.length;
+        k = k % n;
+        //三次反转
+        reverse(nums,0,n-k-1);
+        reverse(nums,n-k,n-1);
+        reverse(nums,0,n-1);
+    }
+    public void reverse(int[] nums,int start,int end){
+        while(start < end){
+            int tmp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = tmp;
+            start++;end--;
         }
     }
-
-    public void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-
-            int tmp = nums[end];
-            nums[end] = nums[start];
-            nums[start] = tmp;
-
-            start++;
-            end--;
-        }
-    }
-
 }
 ```
