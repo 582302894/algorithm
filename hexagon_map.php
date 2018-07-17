2c983d325b79d314e93cd99c72e0d7332cd59058
582302894 <582302894@qq.com>
2018-07-17 14:14:53 +0800

    创建六边形矩阵

--- hexagon_map.php
+++ hexagon_map.php
@@ -1,6 +1,11 @@
 <?php
 
 define('ANGEL_OFFSET', 30);
+// define('ANGEL_OFFSET', 60);
+define('MAP_SIZE', 10);
+define('MAP_LENGTH', 50);
+define('BASE_X', 75);
+define('BASE_Y', 75);
 
 class Point {
     public $x;
@@ -35,10 +40,10 @@ class SixHexagon {
 function getPoint($center, $size, $i, $offset = ANGEL_OFFSET) {
     $angel_deg = 60 * ($i + 1) - $offset;
     $angel_reg = M_PI / 180 * $angel_deg;
-    return new Point(
-        $center->x + $size * cos($angel_reg),
-        $center->y + $size * sin($angel_reg)
-    );
+    //精度到8位
+    $x = round($center->x + $size * cos($angel_reg), 8);
+    $y = round($center->y + $size * sin($angel_reg), 8);
+    return new Point($x, $y);
 }
 
 class PointV {
@@ -49,9 +54,17 @@ class PointV {
         if ($start->x > $end->x) {
             $this->start = $end;
             $this->end = $start;
-        } else {
+        } elseif ($start->x != $end->x) {
             $this->start = $start;
             $this->end = $end;
+        } else {
+            if ($start->y > $end->y) {
+                $this->start = $end;
+                $this->end = $start;
+            } else {
+                $this->start = $start;
+                $this->end = $end;
+            }
         }
         $this->key = implode("_", [
             $this->start->x,
@@ -81,8 +94,12 @@ function getVList($centers) {
     return $v;
 }
 
-function draw($v) {
-    $file = __DIR__ . '/temp/hexagon.png';
+function draw($v, $number = null) {
+    if ($number !== null) {
+        $file = __DIR__ . '/temp/temp/hexagon' . $number . '.png';
+    } else {
+        $file = __DIR__ . '/temp/hexagon.png';
+    }
     $img = imagecreate(1000, 1000);
     imagecolorallocate($img, 255, 255, 255);
     $black = imagecolorresolve($img, 0, 0, 0);
@@ -94,6 +111,39 @@ function draw($v) {
     imagedestroy($img);
 }
 
-$base = new SixHexagon(75, 75, 50);
-$v = getVList([$start]);
+function createMap() {
+    $base = new SixHexagon(BASE_X, BASE_Y, MAP_LENGTH);
+    for ($i = 0; $i < MAP_SIZE; $i++) {
+        for ($j = 0; $j < MAP_SIZE; $j++) {
+            if ($i % 2 == 0) {
+                if ($i == 0 && $j == 0) {
+                    $map[$i][$j] = $base;
+                } else {
+                    $map[$i][$j] = new SixHexagon(BASE_X + $j * cos(30 * M_PI / 180) * MAP_LENGTH * 2, BASE_Y + $i * MAP_LENGTH * 1.5, MAP_LENGTH);
+                }
+            } else {
+                $map[$i][$j] = new SixHexagon(BASE_X + MAP_LENGTH * cos(30 * M_PI / 180) + $j * MAP_LENGTH * 2 * cos(30 * M_PI / 180), BASE_Y + $i * MAP_LENGTH * 1.5, MAP_LENGTH);
+            }
+        }
+    }
+    return $map;
+}
+
+function printVList($v) {
+    $count = 0;
+    foreach ($v as $key => $pointV) {
+        $str[] = sprintf("%' 2d start:%' 23.17f %' 19.17f\tend:%' 19.17f %' 19.17f\tkey:%s", $count, $pointV->start->x, $pointV->start->y, $pointV->end->x, $pointV->end->y, $pointV->key);
+        $count++;
+    }
+    sort($str);
+    echo implode("\n", $str);
+}
+
+$map = createMap();
+foreach ($map as $x) {
+    foreach ($x as $y) {
+        $centers[] = $y;
+    }
+}
+$v = getVList($centers);
 draw($v);
